e2f2cf8084c3f24334203d9cec498a697c83dd5b
AritroChowdhury <71551202+AritroChowdhury@users.noreply.github.com>
2021-07-01 15:26:54 +0530

    Add files via upload

---
+++ pro108.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import plotly.figure_factory
+import csv
+
+df=pd.read_csv('data1.csv')
+fig = ff.create_distplot([df["Avg rating of the brand(No)"].tolist()], ["name of the bell-curve"], show_hist=False)
+fig.show()
